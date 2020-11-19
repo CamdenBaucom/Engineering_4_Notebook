148a8db18b1ad0224f82509754ce3d129acb2354
CamdenBaucom <camdenbaucom1@gmail.com>
2020-11-19 19:31:27 +0000

    working on pinata

---
+++ Python/msp.py
@@ -0,0 +1,14 @@
+#\_
+#< |__
+# | _ |
+# || ||
+def pinata(numWrong):
+	shape = list("\_\n< |__\n | _ |\n || ||")
+	for i in range(0, numWrong):
+		print (shape[i])
+		#shapeConvert = []
+		#shapeConvert.append(shape[i])
+		#print(*shapeConvert)
+		#partShape = "".join(map(str,shapeConvert)
+		#print(partShape)
+pinata(6)
