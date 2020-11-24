4385898f2117623733cb1dac5a9a97ff54c4f0ce
CamdenBaucom <camdenbaucom1@gmail.com>
2020-11-24 19:11:27 +0000

    working on msp

--- Python/msp.py
+++ Python/msp.py
@@ -1,14 +1,15 @@
-#\_
+# _
 #< |__
 # | _ |
 # || ||
 def pinata(numWrong):
-	shape = list("\_\n< |__\n | _ |\n || ||")
-	for i in range(0, numWrong):
-		print (shape[i])
-		#shapeConvert = []
-		#shapeConvert.append(shape[i])
-		#print(*shapeConvert)
-		#partShape = "".join(map(str,shapeConvert)
-		#print(partShape)
-pinata(6)
+	counter = -1
+	shape = [' _\n','< ','|','_','_\n ','| ','_ ','|\n ','|','| ','|','|'] 
+	for x in range(numWrong):
+		counter += 1
+		newShape = shape[counter]
+		print(str(newShape) + "", end='')
+howWrong = int(input("How many wrong"))
+print("\n" * 50)
+pinata(howWrong)
+print("\n" * 30)
