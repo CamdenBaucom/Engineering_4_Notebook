3a94ceb309dc3ac852a13171791185b3b1c22b9f
CamdenBaucom <camdenbaucom1@gmail.com>
2020-12-03 19:19:22 +0000

    finished msp

--- Python/msp.py
+++ Python/msp.py
@@ -2,12 +2,27 @@
 #< |__
 # | _ |
 # || ||
-def pinata(numWrong):
+
+#O__
+#|  |
+#V  V
+
+#O_
+#| |
+
+def pinata(numWrong,howHard):
 	counter = -1
-	shape = [' _\n','< ','|','_','_\n ','| ','_ ','|\n ','|','| ','|','|'] 
+	shape1 = [' _\n','< ','|','_','_\n ','| ','_ ','|\n ','|','| ','|','|'] 
+	shape2 = ['O','_','_\n','|  ','|\n','V  ','V']
+	shape3 = ['O','_\n','| ','|']
 	for x in range(numWrong):
 		counter += 1
-		newShape = shape[counter]
+		if howHard == 1:
+			newShape = shape1[counter]
+		elif howHard == 2:
+			newShape = shape2[counter]
+		else:
+			newShape = shape3[counter]
 		print(str(newShape) + "", end='')
 	print("\n")
 
@@ -46,9 +61,10 @@ def wordSearch(guess):
 			if word[i] == guess:
 				guessPos.append(i)
 		for i in range(len(guessPos)):
-			blankWordList.remove("-")
-		for i in range(len(guessPos)):
+			del blankWordList[guessPos[i]]
 			blankWordList.insert(guessPos[i],guess)
+		#for i in range(len(guessPos)):
+		#	blankWordList.insert(guessPos[i],guess)
 		blankWord = ''.join(map(str, blankWordList))
 		print(blankWord)
 	else:
@@ -56,17 +72,28 @@ def wordSearch(guess):
 		print(blankWord)
 		global numberWrong
 		numberWrong += 1
-		pinata(numberWrong)
+		global difficulty
+		pinata(numberWrong,difficulty)
 
 global logOfGuesses
 logOfGuesses = []
 numberWrong = 0
+difficulty = int(input("Player 2, choose difficulty(how many guesses you get)\nType 1 for easy(12 guesses), 2 for regular(7 guesses), or 3 for hard(4 guesses):\n"))
+while difficulty != 1 and difficulty != 2 and difficulty != 3:
+	difficulty = int(input("Please input either 1,2, or 3:\n"))
 secretWord = input("Player 1, please enter a lowercase word you want Player 2 to guess:\n")
 print("\n" * 50)
 print(codedWord(secretWord))
 word = secretWord
 #print("\n" * 50)
-while numberWrong < 12 and blankWord != secretWord:
+endCrit = 0
+if difficulty == 1:
+	endCrit = 12
+elif difficulty == 2:
+	endCrit = 7
+else:
+	endCrit = 4
+while numberWrong < endCrit and blankWord != secretWord:
 	newGuess = input("Player 2, please guess a letter:\n")
 	#guessLog(newGuess)
 	if guessLog(newGuess) == False:
@@ -75,7 +102,7 @@ while numberWrong < 12 and blankWord != secretWord:
 	else:
 	#	guessLog(newGuess)
 		wordSearch(newGuess)
-if numberWrong == 12:
+if numberWrong == endCrit:
 	print("Sorry, you lost! The answer was " + secretWord)
 if blankWord == secretWord:
 	print("You won, great job!") 
