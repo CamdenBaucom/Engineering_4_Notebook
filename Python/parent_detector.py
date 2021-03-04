df43c5397fc28a336d3d303a9fbd09ed23128376
CamdenBaucom <camdenbaucom1@gmail.com>
2021-03-04 18:35:34 +0000

    Parent detector

--- Python/parent_detector.py
+++ Python/parent_detector.py
@@ -3,10 +3,11 @@ from picamera import PiCamera
 
 pir = MotionSensor(4)
 camera = PiCamera()
+filename = "intruder.h264"
 
 while True:
     pir.wait_for_motion()
     print("Motion Detected")
-    camera.start_preview()
+    camera.start_recording(filename)
     pir.wait_for_no_motion()
-    camera.stop_preview()
+    camera.stop_recording()
